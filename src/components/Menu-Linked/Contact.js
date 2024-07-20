462e31e62d77cb8783bba6f3048a41d486a00ddf
TranTheThanh2k2 <tranthethanh852002@gmail.com>
2024-07-20 16:37:03 +0700

    done11111

--- src/components/Menu-Linked/Contact.js
+++ src/components/Menu-Linked/Contact.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Style/Contact.css'; 
 
 const Contact = () => {
   return (
-    <main>
-    <div className='container' >
-        Contact
-    </div>
-</main>
-  )
+    <main className="contact-main">
+      <div className="contact-container">
+        <h3 className="contact-heading">Địa chỉ</h3>
+        <p className="contact-info">Số 7A-9 Đường số 12, Khu phố 6, P.Bình Chiểu, Thủ Đức, Tp.Hồ Chí Minh</p>
+        <h3 className="contact-heading">Email</h3>
+        <p className="contact-info">service@power5.edu.vn</p>
+      </div>
+    </main>
+  );
 }
 
-export default Contact
+export default Contact;
