4b7c2920531db49de7a8b3cf391a8b68d1a149fb
TranTheThanh2k2 <tranthethanh852002@gmail.com>
2024-07-20 14:37:01 +0700

    done1

---
+++ src/components/Menu-Linked/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <main>
+        <div className='container' >
+            13222222222222222222222222222
+        </div>
+    </main>
+  )
+}
+
+export default AboutUs
---
+++ src/components/Menu-Linked/Person.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Person = () => {
+  return (
+    <main>
+    <div className='container' >
+        Person
+    </div>
+</main>
+  )
+}
+
+export default Person
