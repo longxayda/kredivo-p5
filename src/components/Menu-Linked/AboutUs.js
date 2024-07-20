588fef07bae2367a5717e7f46a24eccab5bc414a
TranTheThanh2k2 <tranthethanh852002@gmail.com>
2024-07-20 16:04:32 +0700

    updateAboutUsateAboutUs

--- src/components/Menu-Linked/AboutUs.js
+++ src/components/Menu-Linked/AboutUs.js
@@ -1,13 +1,88 @@
-import React from 'react'
+import React from "react";
+import "../container.css";
+import "./Style/AboutUs.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import logo1 from "../../Image/1.png";
 
 const AboutUs = () => {
   return (
-    <main>
-        <div className='container' >
-            13222222222222222222222222222
+    <main className="about-us-main container">
+      <div className="about-us-container">
+        <div className="about-us-content">
+          <div className="about-us-intro">
+            <h6>
+              Hành trình khởi đầu của Power5 Technology trước hết là một công ty
+              phát triển công nghệ trong lĩnh vực giáo dục (chuyển đổi số dự án
+              an toàn học đường, nền tảng LMS Power5.edu.vn). Với những thành
+              công đạt được, Power5 tiếp tục mở rộng sang các lĩnh vực công nghệ
+              trọng yếu như Blockchain và AI, đồng thời tham gia vào tiến trình
+              chuyển đổi số của doanh nghiệp.
+            </h6>
+          </div>
+          <div className="about-us-milestones">
+            <h3>Cột mốc chính</h3>
+            <div className="milestone">
+              <div className="milestone-year">
+                <FontAwesomeIcon icon={faStar} />
+                <p>2024</p>
+              </div>
+              <div className="milestone-description">
+                <p>
+                  Trải qua quá trình sáp nhập và phát triển, Power5 Technology
+                  là pháp nhân mới được thành lập độc lập với doanh nghiệp ban
+                  đầu nhưng kế thừa nguồn lực và kết nối vững vàng từ công ty
+                  tiền thân. Là một pháp nhân mới, Power5 Technology còn là biểu
+                  tượng cho sự hợp nhất của trí tuệ, đam mê và khát vọng chinh
+                  phục những thử thách mới.
+                </p>
+              </div>
+            </div>
+            <div className="milestone">
+              <div className="milestone-year">
+                <FontAwesomeIcon icon={faStar} />
+                <p>2018</p>
+              </div>
+              <div className="milestone-description">
+                <p>
+                  Ngọn lửa đam mê công nghệ được khơi dậy bởi đội ngũ kỹ sư đầy
+                  nhiệt huyết và giàu kinh nghiệm. Từ đó, Power5 được thành lập
+                  dưới hình thức Công ty TNHH, đánh dấu bước đầu tiên trên hành
+                  trình công nghệ.
+                </p>
+              </div>
+            </div>
+            <h3>Ban lãnh đạo cấp cao</h3>
+            <div>
+            <div className="leaders">
+              <div className="leader">
+                <div style={{width:'200px' , height:'200px',border:'1px solid #2D947A', borderRadius:'50%'}}><img src={logo1} alt="Leader 1" className="leader-img" /></div>
+                <p>LS Đào Xuân Sơn</p>
+              </div>
+              <div className="leader">
+                  <div style={{width:'200px' , height:'200px',border:'1px solid #2D947A', borderRadius:'50%'}}><img src={logo1} alt="Leader 1" className="leader-img" /></div>
+                <p>LS Đào Tiến Phong</p>
+              </div>
+            </div>
+            <div className="leaders1">
+              <div className="leader">
+              <div style={{width:'200px' , height:'200px',border:'1px solid #2D947A', borderRadius:'50%'}}><img src={logo1} alt="Leader 1" className="leader-img" /></div>                <p>TS Hàng Sấm Nang</p>
+              </div>
+              <div className="leader">
+              <div style={{width:'200px' , height:'200px',border:'1px solid #2D947A', borderRadius:'50%'}}><img src={logo1} alt="Leader 1" className="leader-img" /></div>                <p>Ông Hồ Vân Long</p>
+              </div>
+              <div className="leader">
+              <div style={{width:'200px' , height:'200px',border:'1px solid #2D947A', borderRadius:'50%'}}><img src={logo1} alt="Leader 1" className="leader-img" /></div>                
+              <span style={{color:'green'}}>Kỹ sư công nghệ thông tin</span>
+              <p>Hà Huy Hiệu</p>
+              </div>
+            </div>
+            </div>
+          </div>
         </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
